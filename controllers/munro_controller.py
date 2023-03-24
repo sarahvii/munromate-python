49e2635fafa962822c1f6e2716ab0d04d8fab5f9
Sarah Smith <sarahsmithvii@gmail.com>
2023-03-24 16:40:00 +0000

    /munros GET request

--- controllers/munro_controller.py
+++ controllers/munro_controller.py
@@ -1,7 +1,9 @@
 from flask import render_template
+from repositories import munro_repository
 from flask import Blueprint
 munros_blueprint = Blueprint("munros", __name__)
 
 @munros_blueprint.route('/munros')
-def index():
-    return render_template('munros/index.html', title='Munros')
+def munros():
+    munros = munro_repository.select_all()
+    return render_template('munros/index.html', title='Munros', all_munros = munros)
