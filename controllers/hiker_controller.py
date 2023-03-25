ecbdc3cda157ce5b08b5099f277bfffc07533694
Sarah Smith <sarahsmithvii@gmail.com>
2023-03-25 14:03:32 +0000

    GET /hiker/new route created to return new hiker form

--- controllers/hiker_controller.py
+++ controllers/hiker_controller.py
@@ -13,8 +13,12 @@ def hikers():
 
 # NEW
 # GET '/hikers/new'
-# FORM TO ADD NEW HIKER
+# FORM TO ADD NEW HIKER / RETURNS A HTML FORM TO THE BROWSER
+@hikers_blueprint.route('/hikers/new', methods=['GET'])
+def new_hiker():
+    return render_template("hikers/new.html")
 
 # CREATE
 # POST '/hikers/'
 # When the user submits the form we'll make a POST request to the CREATE route
+# Receives the data from the form to insert into the database
